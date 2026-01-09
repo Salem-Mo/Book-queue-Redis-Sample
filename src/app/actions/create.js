2603e266c645904e7dbf3b3e4e79625dafbf7544
Salem-Mo <124922909+Salem-Mo@users.noreply.github.com>
2026-01-10 01:47:12 +0200

    using queue

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -9,6 +9,15 @@ export async function createBook(formData) {
   const id = Math.floor(Math.random()*100000)
 
 
+  const unique =await client.zAdd('books',
+    {
+      value:title,
+      score:id
+    },{NX:true}
+  )
+if(!unique){
+  return {error: "The Book is already added"}
+}
 
   await client.hSet(`books:${id}`,{
       title,
